7be1196ac4866a702fa00eae8ac195e5aee4506d
iainrwolfheart <iainrgorrie@gmail.com>
2021-01-17 19:37:12 +0000

    Added notes to build homepage and other functionality

--- asides_app/screens/HomeScreen.js
+++ asides_app/screens/HomeScreen.js
@@ -1,26 +1,21 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet, Text, Button} from 'react-native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getUserToken } from '../services/AuthService';
 
 export default function HomeScreen({route, navigation}) {
 
-    // const getUserToken = async() => {
-    //     try {
-    //         const retrievedToken = await AsyncStorage.getItem("userToken");
-    //         if (retrievedToken != null) {
-    //             console.log("getUserToken: " + retrievedToken)
-    //             return retrievedToken;
-    //         } else {
-    //             // Complete once happy path working   
-    //             // Probably redirect to WelcomeScreen w/logged out/error msg?  
-    //         }
-    //     } catch (error) {
-    //         console.dir('Token Retrievel Error: ' + error.message);
-    //     }   
-    // }
     const authToken = getUserToken(); //currently not working 100% via getUserToken - rtrng object instead String?
     console.log("Home screen auth: " + authToken);
+
+
+    // FUNCTIONALITY
+    // Organise a game - OrganiseMatchScreen
+    // Show last match data
+    // Rate teammates following last match
+    // Show next match detail / organise prompt
+    // Show group name
+    // Show logged in player's name
+    // Burger bar to update profile, logout
 
     return (
         <SafeAreaView>
